c5525f6e9029384f135339abf113ae5424b249f4
ritu2005425 <66471042+ritu2005425@users.noreply.github.com>
2020-06-05 23:14:12 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-   int n,r,s=0;
+   int n,s=0;
    printf("enter number");
    scanf("%d",&n);
  
    while(n>0)
    {
 
-       r=n%10;
+      int r=n%10;
        s=s+r;
        n=n/10;
 
